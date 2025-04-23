576066315fd6398a63191114669777c0b4644578
buyaramadhan <ytchochote@gmail.com>
2025-04-23 21:17:15 +0700

    mengatur struktur dasar aplikasi

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+import 'package:loginpage/homepage.dart';
+import 'package:loginpage/login.dart';
+import 'package:loginpage/orderpage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
