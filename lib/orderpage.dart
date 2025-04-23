666186563fe860c3a95df4fb5eeeac41d69b7fa9
buyaramadhan <ytchochote@gmail.com>
2025-04-23 20:48:02 +0700

    menambahkan halaman OrderPage

---
+++ lib/orderpage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:loginpage/detailorder.dart'; 
+
+class OrderPage extends StatefulWidget {
+  const OrderPage({super.key});
+
+  @override
+  State<OrderPage> createState() => _OrderPageState();
+}
+
