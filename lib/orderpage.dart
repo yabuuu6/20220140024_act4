aba6878f3172f9eb07824f96b9fce443d2856800
buyaramadhan <ytchochote@gmail.com>
2025-04-23 20:49:28 +0700

    menambahkan perhitungan total harga

--- lib/orderpage.dart
+++ lib/orderpage.dart
@@ -8,3 +8,23 @@ class OrderPage extends StatefulWidget {
   State<OrderPage> createState() => _OrderPageState();
 }
 
+class _OrderPageState extends State<OrderPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController makananController = TextEditingController();
+  final TextEditingController minumanController = TextEditingController();
+  final TextEditingController jumlahMakananController = TextEditingController();
+  final TextEditingController jumlahMinumanController = TextEditingController();
+
+  int totalHarga = 0;
+
+  void calculateTotalPrice() {
+    int jumlahMakanan = int.tryParse(jumlahMakananController.text) ?? 0;
+    int jumlahMinuman = int.tryParse(jumlahMinumanController.text) ?? 0;
+
+    setState(() {
+      totalHarga = (jumlahMakanan * 32000) + (jumlahMinuman * 5000);
+    });
+  }
+
+ 
