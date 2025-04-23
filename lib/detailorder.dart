c8eb69ff55470afed22548f682858d0e2c77e0fa
buyaramadhan <ytchochote@gmail.com>
2025-04-23 20:52:17 +0700

    menambahkan halaman DetailOrderPage untuk menampilkan rincian pesanan

---
+++ lib/detailorder.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:loginpage/homepage.dart';
+
+class DetailOrderPage extends StatelessWidget {
+  final String makanan;
+  final String minuman;
+  final String jumlahMakanan;
+  final String jumlahMinuman;
+  final int totalHarga;
+
+  const DetailOrderPage({
+    super.key,
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMakanan,
+    required this.jumlahMinuman,
+    required this.totalHarga,
+  });
+
+ 
