3ffd8e4c18d7017728f396f161c509e9a763759a
buyaramadhan <ytchochote@gmail.com>
2025-04-23 20:34:21 +0700

    menambahkan validasi form login dan navigasi ke homepage

--- lib/login.dart
+++ lib/login.dart
@@ -18,4 +18,24 @@ class _LoginPageState extends State<LoginPage> {
     super.initState();
   }
 
- 
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
