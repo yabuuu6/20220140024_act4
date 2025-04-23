342ea0f3c067ccd953ce4a10607e25779762b90c
buyaramadhan <ytchochote@gmail.com>
2025-04-23 20:31:08 +0700

    menambahkan halaman Login dengan input email dan password

---
+++ lib/login.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:loginpage/homepage.dart';
+import 'package:loginpage/register.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+ 
