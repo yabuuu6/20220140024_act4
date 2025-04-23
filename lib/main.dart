529dd6e00905d275f90057118e943b5820492e4e
buyaramadhan <ytchochote@gmail.com>
2025-04-23 21:17:50 +0700

    mengatur navigasi awal dan routing antar halaman

--- lib/main.dart
+++ lib/main.dart
@@ -11,4 +11,19 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/home': (context) => const HomePage(),
+        '/order': (context) => const OrderPage(),
+      },
+    );
+  }
+}
