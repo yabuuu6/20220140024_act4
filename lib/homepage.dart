59de8d626bd79fceaa0655a2b2e79a6d2bbb377e
buyaramadhan <ytchochote@gmail.com>
2025-04-23 20:44:49 +0700

    menambahkan halaman utama

---
+++ lib/homepage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:loginpage/orderpage.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
